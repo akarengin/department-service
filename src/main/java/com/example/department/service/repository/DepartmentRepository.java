433718b303c02e07e9d28e7ab2e8ff80f40a29c8
akarengin <engin.akar1@gmail.com>
2023-03-14 14:09:00 +0300

    first commit

---
+++ src/main/java/com/example/department/service/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.example.department.service.repository;
+
+import com.example.department.service.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
